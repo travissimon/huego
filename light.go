6f3e53aa7e5eab73c6f5a59abf29b6382ada6d0f
tsimon <tsimon@gmail.com>
2014-04-13 14:38:22 +1000

    Work on updating a light state

--- light.go
+++ light.go
@@ -1,12 +1,17 @@
 package Huego
 
+import (
+	"bytes"
+	"fmt"
+)
+
 type State struct {
 	On        bool
 	Hue       uint16
 	Bri       uint8
 	Sat       uint8
-	X         float
-	Y         float
+	X         float32
+	Y         float32
 	Ct        uint16
 	Alert     string
 	Effect    string
@@ -28,4 +33,92 @@ func NewLight() *Light {
 	light.prevState = new(State)
 	light.State = new(State)
 	return light
+}
+
+func (l *Light) ResetState() {
+	l.prevState.On = l.State.On
+	l.prevState.Hue = l.State.Hue
+	l.prevState.Bri = l.State.Bri
+	l.prevState.Sat = l.State.Sat
+	l.prevState.X = l.State.X
+	l.prevState.Y = l.State.Y
+	l.prevState.Ct = l.State.Ct
+	l.prevState.Alert = l.State.Alert
+	l.prevState.Effect = l.State.Effect
+	l.prevState.Colormode = l.State.Colormode
+	l.prevState.Reachable = l.State.Reachable
+}
+
+func (l *Light) SetStateWithTransition(transitionTime uint16) string {
+	return l.setStateInternal(int32(transitionTime))
+}
+
+func (l *Light) SetState() string {
+	return l.setStateInternal(-1)
+}
+
+func (l *Light) setStateInternal(transitionTime int32) string {
+	var buffer bytes.Buffer
+	fieldsUpdated := false
+
+	if l.State.On != l.prevState.On {
+		l.writeUpdateParam(&buffer, "on", boolToString(l.State.On), fieldsUpdated)
+		fieldsUpdated = true
+	}
+	if l.State.Bri != l.prevState.Bri {
+		l.writeUpdateParam(&buffer, "bri", string(l.State.Bri), fieldsUpdated)
+		fieldsUpdated = true
+	}
+	if l.State.Hue != l.prevState.Hue {
+		l.writeUpdateParam(&buffer, "hue", string(l.State.Hue), fieldsUpdated)
+		fieldsUpdated = true
+	}
+	if l.State.X != l.prevState.X || l.State.Y != l.prevState.Y {
+		l.writeUpdateParam(&buffer, "xy", "["+floatToString(l.State.X)+","+floatToString(l.State.Y)+"]", fieldsUpdated)
+		fieldsUpdated = true
+	}
+	if l.State.Ct != l.prevState.Ct {
+		l.writeUpdateParam(&buffer, "ct", string(l.State.Ct), fieldsUpdated)
+		fieldsUpdated = true
+	}
+	if l.State.Alert != l.prevState.Alert {
+		l.writeUpdateParam(&buffer, "alert", stringToString(l.State.Alert), fieldsUpdated)
+		fieldsUpdated = true
+	}
+	if l.State.Effect != l.prevState.Effect {
+		l.writeUpdateParam(&buffer, "effect", stringToString(l.State.Effect), fieldsUpdated)
+		fieldsUpdated = true
+	}
+	if transitionTime >= 0 {
+		l.writeUpdateParam(&buffer, "transitiontime", string(transitionTime), fieldsUpdated)
+		fieldsUpdated = true
+	}
+	l.ResetState()
+
+	return buffer.String()
+}
+
+func boolToString(b bool) string {
+	if b {
+		return "true"
+	} else {
+		return "false"
+	}
+}
+
+func floatToString(f float32) string {
+	return fmt.Sprintf("%6.2f", f)
+}
+
+func stringToString(s string) string {
+	return "\"" + s + "\""
+}
+
+func (l *Light) writeUpdateParam(buffer *bytes.Buffer, p string, val string, inclPrecedingComma bool) {
+	if inclPrecedingComma {
+		buffer.WriteString(",")
+	}
+	buffer.WriteString("\n\t\"")
+	buffer.WriteString("\": ")
+
 }
